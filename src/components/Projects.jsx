bcde06734306ed3d4bf4c627e4c73eaeed8f0e60
Lutsun <sergedasylva0411@gmail.com>
2025-08-27 20:42:27 +0000

    update add image for employeemanagement

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,7 +28,7 @@ const projects = [
     description: "A simple Java desktop application to manage employees with a graphical interface.",
     tags: ["Java", "Sqlite"],
     link: "https://github.com/Lutsun/Project_Java_Exam.git",
-    image: "https://images.unsplash.com/photo-1555529669-e69e7aa0ba9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+    image: "../assets/images/Employeemanagement.png"
   }
 ];
 
